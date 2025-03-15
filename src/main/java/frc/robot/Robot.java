d8d6c513b89a7153f9c3972b7211351872e7e437
QC-Elite-648 <qcelite@qcelite.com>
2025-03-15 13:16:14 -0500

    presets and camera

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,8 +7,11 @@ package frc.robot;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+//adding the camera service
+import edu.wpi.first.cameraserver.CameraServer;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to each mode, as
@@ -48,6 +51,9 @@ public class Robot extends TimedRobot
     // Create a timer to disable motor brake a few seconds after disable.  This will let the robot stop
     // immediately when disabled, but then also let it be pushed more 
     disabledTimer = new Timer();
+
+    //start streaming the camera feed
+    CameraServer.startAutomaticCapture();
 
     if (isSimulation())
     {
--- src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
@@ -2,10 +2,12 @@ package frc.robot.subsystems.elevator;
 
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -14,18 +16,47 @@ import frc.robot.Constants.CANConfig;
 public class ElevatorSubsystem extends SubsystemBase
 {
     private final SparkMax elevator1 = new SparkMax(CANConfig.ELEVATOR_LEFT, MotorType.kBrushless);
-    private final SparkMax elevator2 = new SparkMax(CANConfig.ELEVATOR_RIGHT, MotorType.kBrushless);;
+    private final SparkMax elevator2 = new SparkMax(CANConfig.ELEVATOR_RIGHT, MotorType.kBrushless);
+    private final SparkMax endEffectorTilt = new SparkMax(CANConfig.END_EFFECTOR_TILT, MotorType.kBrushless);
 
     private SparkMaxConfig elevator1Config = new SparkMaxConfig();
     private SparkMaxConfig elevator2Config = new SparkMaxConfig();
+
+    private double startingHeight = 15.25;
+    private double endingHeight = 59.25;
+    private double startingEncoder = 0;
+    private double endingEncoder = 110;
+
+    private double inchesPerEncoder = (endingHeight - startingHeight)/(endingEncoder - startingEncoder);
+
+    private PIDController elevatorPID = new PIDController(.5,0,0);
     
     public ElevatorSubsystem()
     {
         elevator1Config.inverted(true);
         elevator2Config.inverted(false);
+        elevator1Config.idleMode(IdleMode.kBrake);
+        elevator2Config.idleMode(IdleMode.kBrake);
         elevator1.configure(elevator1Config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
         elevator2.configure(elevator2Config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        elevatorPID.setTolerance(1);
     }
+
+    public void goToHeight(double height){
+        height = height - 15.25; //15.25 is starting height
+        double voltage = elevatorPID.calculate(this.getHeight(), height);
+        if(Math.abs(voltage) > 3){
+            voltage = (voltage)/Math.abs(voltage)*3;
+        }
+        SmartDashboard.putNumber("elevator PID Voltage", voltage);
+        elevator1.setVoltage(voltage);
+        elevator2.setVoltage(voltage);
+
+    }
+    public double getHeight(){
+        return elevator1.getEncoder().getPosition()*inchesPerEncoder;
+    }
+
 
     public void setSpeedElevator1(double speed) 
     {
@@ -37,10 +68,17 @@ public class ElevatorSubsystem extends SubsystemBase
         elevator2.set(speed);
     }
 
+    public void setSpeedEndEffectorTilt(double speed){
+        endEffectorTilt.set(speed);
+    }
+
     public void setSpeed(double speed){
         elevator1.set(speed);
         elevator2.set(speed);
     }
+
+    //elevator positions
+    
 
     @Override
     public void periodic()
